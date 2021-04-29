aa450901e18bbc3beb5014b24f38d4dd08e706ab
Vikas Pal <rohitprotobit@gmail.com>
2021-04-29 12:18:56 +0530

    category display

--- category/models.py
+++ category/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    name = models.CharField(max_length=100)
     description = models.TextField(max_length=255, blank=True)
     cate_image = models.ImageField(upload_to="photos/categories", blank=True)
 
