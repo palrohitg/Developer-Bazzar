89db8fbe41bf2efbb26b75203c6266724475eb4a
Vikas Pal <rohitprotobit@gmail.com>
2021-05-05 14:00:44 +0530

    navbar cart option adde

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
 
 
 def store(request, category_slug=None):
@@ -27,11 +30,14 @@ def product_details(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
-        'single_product': single_product
+        'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
