9d3dc93ddab201716099b9f5ed353a12f2bb7717
Vikas Pal <rohitprotobit@gmail.com>
2021-05-07 11:04:21 +0530

    search function added

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>',
-         views.product_details, name='product_details')
-
+    path('category/<slug:category_slug>/',
+         views.store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>',
+         views.product_details, name='product_details'),
+    path('search', views.search, name="search"),
 ]
--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from django.http import HttpResponse
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.db.models import Q
 
 
 def store(request, category_slug=None):
@@ -19,7 +20,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 3)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -48,3 +49,17 @@ def product_details(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by(
+                '-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
