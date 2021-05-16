4e008479f99f22dad64c7300fc11be70ea7b22ce
Vikas Pal <rohitprotobit@gmail.com>
2021-05-16 23:45:30 +0530

    login feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
 
 
 def register(request):
@@ -31,6 +31,19 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        print(email + " " + password)
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid Login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
 }); 
 // jquery end
 
-setTimeout(function(){
-    $('#message').fadeOut('slow')
 
+setTimeout(function(){
+  $('#message').fadeOut('slow')
 }, 4000)
