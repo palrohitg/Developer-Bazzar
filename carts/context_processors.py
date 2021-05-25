285764a6b105504501c4d814d048602cdd645662
Vikas Pal <rohitprotobit@gmail.com>
2021-05-25 21:37:00 +0530

    preprocessor added

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,16 +1,17 @@
 from .models import Cart, CartItem
-from .views import _cart_id 
+from .views import _cart_id
+
 
 def counter(request):
     cart_count = 0
     if 'admin' in request.path:
-        return {} 
-    else :
-        try :
-            cart = Cart.objects.filter(cart_id = _cart_id(request)) 
-            cart_item = CartItem.objects.all().filter(cart = cart[:1])
+        return {}
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_item:
-                cart_count += cart_item.quantity 
+                cart_count += cart_item.quantity
         except Cart.DoesNotExists:
-            cart_count = 0 
-    return dict(cart_count = cart_count)
+            cart_count = 0
+    return dict(cart_count=cart_count)
