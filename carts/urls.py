4d65f6679fffbc5e9f92e63e0b81dab7a63fa022
Vikas Pal <rohitprotobit@gmail.com>
2021-06-22 18:14:07 +0530

    remove card urls updated

--- carts/urls.py
+++ carts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
          views.remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/',
          views.remove_cart_item, name='remove_cart_item'),
+
     path('checkout/', views.checkout, name='checkout'),
 ]
